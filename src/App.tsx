43a60a78e713881f20cba280671242d6b770acd2
koladev <onaelmangabo@gmail.com>
2021-08-21 17:08:12 +0100

    - feat: wrap App around store

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
 import React from 'react';
-import {Button, Form} from 'react-bootstrap'
 import './App.css';
-import CustomNavbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store";
 
 function App() {
 return (
-<div className="App">
-
-  <CustomNavbar />
-
-  <div className="container">
-    <div>
-      <h3 className="text-center">Join meeting</h3>
-      <Form className="col-md-4 col-sm-8 mx-auto">
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label className="text-muted">Meeting ID</Form.Label>
-          <Form.Control type="text" placeholder="Enter meeting ID" />
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>By clicking "Join", you agree to our <a href="#">Terms of Services</a> and <a href="">Privacy Statement</a></Form.Label>
-        </Form.Group>
-        <Button className="w-100 rounded-3" type="submit">Join</Button>
-      </Form>
-    </div>
-  </div>
-</div>
+  <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null}>
+      <div>
+        Welcome
+      </div>
+    </PersistGate>
+  </Provider>
 );
 }
 
