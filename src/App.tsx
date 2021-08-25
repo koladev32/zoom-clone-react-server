4111ebc064793f9aca72a9b94f90ab79331f54a3
koladev <onaelmangabo@gmail.com>
2021-08-25 21:50:55 +0100

    - wip: wrap App around Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import store, { persistor } from "./store";
+import {Route, Switch, BrowserRouter as Router} from "react-router-dom";
+import Meeting from "./pages/Meeting";
 
 function App() {
 return (
-  <Provider store={store}>
-    <PersistGate persistor={persistor} loading={null}>
-      <div>
-        Welcome
-      </div>
-    </PersistGate>
-  </Provider>
+      <Router>
+          <div className="App">
+            <Switch>
+              <Route exact path='/' component={Meeting} />
+              <Route path='/host' component={() => <div>Hello</div>} />
+            </Switch>
+          </div>
+      </Router>
 );
 }
 
