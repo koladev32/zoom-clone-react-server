0b06154a9482c2349af30ee99d824eb93a946212
koladev <onaelmangabo@gmail.com>
2021-08-25 21:52:59 +0100

    - feat: add username input

--- src/components/RoomForm.tsx
+++ src/components/RoomForm.tsx
@@ -2,23 +2,28 @@ import React from "react";
 import {Button, Form} from "react-bootstrap";
 
 
-function RoomForm () {
-    return (
-        <div>
-            <h3 className="text-center">Join meeting</h3>
-            <Form className="col-md-4 col-sm-8 mx-auto">
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label className="text-muted">Meeting ID</Form.Label>
-                    <Form.Control type="text" placeholder="Enter meeting ID" />
-                </Form.Group>
+function RoomForm() {
+  return (
+    <div>
+      <h3 className="text-center">Join meeting</h3>
+      <Form className="col-md-4 col-sm-8 mx-auto">
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label className="text-muted">Meeting ID</Form.Label>
+          <Form.Control type="text" placeholder="Enter meeting ID" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+          <Form.Label className="text-muted">Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter a username" />
+        </Form.Group>
 
-                <Form.Group className="mb-3">
-                    <Form.Label>By clicking "Join", you agree to our <a href="#">Terms of Services</a> and <a href="">Privacy Statement</a></Form.Label>
-                </Form.Group>
-                <Button className="w-100 rounded-3" type="submit">Join</Button>
-            </Form>
-        </div>
-    )
+        <Form.Group className="mb-3">
+          <Form.Label>By clicking "Join", you agree to our <a href="#">Terms of Services</a> and <a href="">Privacy
+            Statement</a></Form.Label>
+        </Form.Group>
+        <Button className="w-100 rounded-3" type="submit">Join</Button>
+      </Form>
+    </div>
+  );
 }
 
 export default RoomForm;
