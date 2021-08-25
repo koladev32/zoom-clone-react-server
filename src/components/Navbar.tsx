c3e14a625d96b5114a834afbd228d99d60b58636
koladev <onaelmangabo@gmail.com>
2021-08-25 21:57:05 +0100

    - feat: add routes to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Container, Nav, NavDropdown, Navbar} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 function CustomNavbar () {
 
@@ -10,14 +11,12 @@ function CustomNavbar () {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
                     <Nav>
-                        <Nav.Link className="text-primary" href="#features">JOIN A MEETING</Nav.Link>
-                        <NavDropdown className="text-primary" title="HOST A MEETING" id="collasible-nav-dropdown">
-                            <NavDropdown.Item className="text-primary" href="#action/3.1">JOIN A MEETING</NavDropdown.Item>
-                            <NavDropdown.Item className="text-primary" href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item className="text-primary" href="#action/3.3">Something</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link className="text-primary" href="#features">SIGN IN</Nav.Link>
-                        <Nav.Link className="btn btn-warning text-white sign-up" href="#features">SIGN UP, IT'S FREE</Nav.Link>
+                        <Nav.Link className="text-primary" href="#features">
+                            <Link to="/">JOIN A MEETING</Link>
+                        </Nav.Link>
+                        <Nav.Link className="text-primary" href="#features">
+                            <Link to="/request/">HOST A MEETING</Link>
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
