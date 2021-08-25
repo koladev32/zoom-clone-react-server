9b22c58f48393a728168ef01c3dfa9e3e8f24987
koladev <onaelmangabo@gmail.com>
2021-08-25 21:50:46 +0100

    - wip: add Meeting Page

---
+++ src/pages/Meeting.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+import RoomForm from "../components/RoomForm";
+
+function Meeting () {
+
+  return (
+    <Layout>
+      <RoomForm />
+    </Layout>
+  )
+}
+
+export default Meeting;
